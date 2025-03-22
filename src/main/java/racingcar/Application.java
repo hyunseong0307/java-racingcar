fde9511752ed2e257cc8b0a2296329769af7112e
hyunseong0307 <hslee000307@gmail.com>
2025-03-22 15:15:09 +0900

    경주 결과 현재 차량위치 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,50 +10,60 @@ public class Application {
     public static void main(String[] args) {
         List<Car> cars = inputCarNames();
         int attempts = inputAttemptCount();
+        playGames(cars, attempts);
 
     }
-    private static List<Car> inputCarNames(){
+
+    private static List<Car> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String input = Console.readLine();
         String[] carNames = input.split(",");
         List<Car> cars = new ArrayList<>();
 
-        for(String carName : carNames){
+        for (String carName : carNames) {
             cars.add(new Car(carName));
         }
         return cars;
     }
 
-    private static int inputAttemptCount(){
+    private static int inputAttemptCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        try{
+        try {
             return Integer.parseInt(Console.readLine());
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자를 입력해야 합니다.");
         }
     }
 
     //차 움직이기
-    private static void playGames(List<Car> cars, int attempts){
-        for(int i = 0; i < attempts; i++){
-            for(Car car : cars){
+    private static void playGames(List<Car> cars, int attempts) {
+        for (int i = 0; i < attempts; i++) {
+            for (Car car : cars) {
                 car.move();
             }
+            printCurrentCarPosition(cars);
         }
     }
-
+    private static void printCurrentCarPosition(List<Car> cars) {
+        for(Car car : cars) {
+            System.out.print(car.getName() + " : ");
+            car.printSlash();
+        }
+        System.out.println();
+    }
 }
-class Car{
+
+class Car {
     private final String carName;
     private int position;
 
-    public Car(String carName){
+    public Car(String carName) {
         this.carName = carName;
         this.position = 0;
     }
 
-    public void move(){
-        if(Randoms.pickNumberInRange(0,9)>=4)
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4)
             position++;
     }
 
@@ -64,6 +74,14 @@ class Car{
     public String getName() {
         return carName;
     }
+
+    public void printSlash(){
+        for(int i=0; i < position ; i++) {
+            System.out.print("- ");
+        }
+        System.out.println();
+    }
 }
+
 
 
